9bd35b6035e6b4e378399d8b54ea35ac85458f52
emma2625 <emmanuelodobo10@gmail.com>
2022-03-28 15:56:08 +0100

    2022-03-28

--- assets/includes/main-nav.php
+++ assets/includes/main-nav.php
@@ -1,4 +1,5 @@
 <section>
+    <link rel="stylesheet" href="../css/styles.css">
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
             <a class="navbar-brand text-uppercase fw-bold" href="index">
@@ -19,6 +20,21 @@
                         <a class="nav-link active" aria-current="page" href="#">Contact</a>
                     </li>
                     <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="tel:+2348142237388">
+                            <i class="fas fa-phone-alt"></i>08142237388
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="mailto:emmanuelodobo10@gmail.com">
+                            <i class="fas fa-envelope"></i>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" target="_blank"  href="https://api.whatsapp.com/send?text=I would like to enquire on the number of rooms that i can book in a month&phone=+2347087777367">
+                            <i class="fab fa-whatsapp"></i>
+                        </a>
+                    </li>
+                    <li class="nav-item">
                         <a class="nav-link active border" aria-current="page" href="authentication">Get Started <i class="fas fa-user"></i></a>
                     </li>
                 </ul>
@@ -26,3 +42,16 @@
         </div>
     </nav>
     </section>
+    <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/6241b8ca0bfe3f4a87700f59/1fv8bjn69';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
